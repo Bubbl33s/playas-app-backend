9f0b6685bf6d79976a36301c920e43fa1728a7c6
Bubbl33s <jairo.lf.8@gmail.com>
2024-11-26 09:44:02 -0500

    feat: add valdiate data middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,5 @@
 import errorHandler from "./errorHandler.middleware";
 import upload from "./upload.middleware";
+import validateData from "./dataValidate.middleware";
 
-export { errorHandler, upload };
+export { errorHandler, upload, validateData };
