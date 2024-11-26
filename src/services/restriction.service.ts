6cb11708325c522a382ae7c92ea4be25bfce2622
Bubbl33s <jairo.lf.8@gmail.com>
2024-11-26 10:22:07 -0500

    feat: add restriction endpoints

--- src/services/restriction.service.ts
+++ src/services/restriction.service.ts
@@ -1,4 +1,5 @@
 import prisma from "../config/prisma.config";
+import { Restriction } from "../types/restriction.types";
 
 export class RestrictionService {
   static async getRestrictions() {
@@ -8,6 +9,30 @@ export class RestrictionService {
   static async getRestriction(id: string) {
     return await prisma.restriction.findUnique({
       where: { id },
+    });
+  }
+
+  static async createRestriction(data: Restriction) {
+    return await prisma.restriction.create({
+      data,
+    });
+  }
+
+  static async updateRestriction(id: string, data: Restriction) {
+    return await prisma.restriction.update({
+      where: { id },
+      data,
+    });
+  }
+  static async deleteRestriction(id: string) {
+    return await prisma.$transaction(async (prisma) => {
+      await prisma.beachRestriction.deleteMany({
+        where: { restrictionId: id },
+      });
+
+      return await prisma.restriction.delete({
+        where: { id },
+      });
     });
   }
 }
